64e36212ee19b3a9274b20f1a231adac69a4f2ff
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2023-11-25 16:54:21 +0530

    added javascript animation

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+const textElement = document.querySelector("#self");
+const name = ['PRASHANT KUSHWAHA', ' A DEVLOPER'];
 
+function animationText(index) {
+    textElement.innerHTML = "I AM "+ name[index];
+    setTimeout ( () => {
+        animationText((index + 1) %name.length);
+    }, 3000);
+
+  
+}
+animationText(0);
 
